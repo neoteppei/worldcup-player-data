ea5f8cc79c79d10baced66096da81a970b2f9059
杉山哲平 <sugiteppei@icloud.com>
2024-07-11 23:37:41 +0900

    Add files via upload

---
+++ resources/views/playercontroller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Player;
+
+class PlayerController extends Controller
+{
+    protected $player;
+
+    public function __construct(Player $player)
+    {
+        $this->player = $player;
+    }
+
+    // 選手一覧画面を表示
+    public function index()
+    {
+        $players = $this->player->all(); // 全ての選手データを取得
+        return view('players.index', ['players' => $players]);
+    }
+
+    // 選手詳細を表示
+    public function detail($id)
+    {
+        $player = $this->player->find($id);
+        return view('players.detail', ['player' => $player]);
+    }
+}
