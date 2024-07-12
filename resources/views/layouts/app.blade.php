bfeaff7cbfce93ddfd17cc2cb3aa3017181fe553
杉山哲平 <sugiteppei@icloud.com>
2024-07-12 18:52:04 +0900

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -13,20 +12,4 @@
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', '選手情報管理')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <div class="container">
-        @yield('content')
-    </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
->>>>>>> 04a1a7bf2af2daf13de46401cdf7ccb53c151cf2
 </html>
