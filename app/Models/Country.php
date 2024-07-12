57227388ee5527deffeb8d5961d640bd130e643e
杉山哲平 <sugiteppei@icloud.com>
2024-07-12 18:46:48 +0900

    Add files via upload

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Models;
@@ -12,18 +11,3 @@ class Country extends Model
 
     protected $fillable = ['name', 'ranking', 'group_name'];
 }
-=======
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Country extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['name', 'ranking', 'group_name'];
-}
->>>>>>> 04a1a7bf2af2daf13de46401cdf7ccb53c151cf2
