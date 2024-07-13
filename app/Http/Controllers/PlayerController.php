f109b77570548b53171171cc0ee5c0aaac27c16a
杉山哲平 <sugiteppei@icloud.com>
2024-07-13 12:17:36 +0900

    Add files via upload

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -10,13 +10,13 @@ class PlayerController extends Controller
 {
     public function index()
     {
-        $players = Player::active()->paginate(20);
+        $players = Player::allPlayers();
         return view('players.index', ['players' => $players]);
     }
 
     public function detail($id)
     {
-        $player = Player::with(['goals.pairing.enemyCountry'])->where('del_flg', 0)->find($id);
+        $player = Player::with(['goals.pairing.enemyCountry', 'country'])->where('del_flg', 0)->find($id);
 
         if (!$player) {
             return redirect()->route('player.index')->with('error', '選手が見つかりません。');
@@ -64,26 +64,37 @@ class PlayerController extends Controller
     }
 
     public function update(Request $request, $id)
-    {
-        $validatedData = $request->validate([
-            'uniform_num' => 'required|integer|numeric',
-            'position' => 'required|string',
-            'name' => 'required|string',
-            'country_id' => 'required|integer|exists:countries,id',
-            'club' => 'required|string',
-            'birth' => 'required|date_format:Y-m-d',
-            'height' => 'required|integer|numeric',
-            'weight' => 'required|integer|numeric',
-        ], [
-            'required' => 'この項目は必須入力です',
-            'integer' => 'この項目は半角数字で入力してください',
-            'numeric' => 'この項目は半角数字で入力してください',
-            'date_format' => 'この項目は「YYYY-MM-DD」で入力してください',
-        ]);
+{
+    $validatedData = $request->validate([
+        'uniform_num' => 'required|integer|numeric',
+        'position' => 'required|string',
+        'name' => 'required|string',
+        'country_id' => 'required|integer|exists:countries,id',
+        'club' => 'required|string',
+        'birth' => 'required|date_format:Y-m-d',
+        'height' => 'required|integer|numeric',
+        'weight' => 'required|integer|numeric',
+    ], [
+        'required' => 'この項目は必須入力です',
+        'integer' => 'この項目は半角数字で入力してください',
+        'numeric' => 'この項目は半角数字で入力してください',
+        'date_format' => 'この項目は「YYYY-MM-DD」で入力してください',
+    ]);
 
-        $player = Player::where('del_flg', 0)->findOrFail($id);
-        $player->update($validatedData);
+    $player = Player::where('del_flg', 0)->findOrFail($id);
 
-        return redirect()->route('player.edit', $id)->with('success', '選手情報が更新されました。');
-    }
+
+    $player->uniform_num = $validatedData['uniform_num'];
+    $player->position = $validatedData['position'];
+    $player->name = $validatedData['name'];
+    $player->country_id = $validatedData['country_id'];
+    $player->club = $validatedData['club'];
+    $player->birth = $validatedData['birth'];
+    $player->height = $validatedData['height'];
+    $player->weight = $validatedData['weight'];
+
+    $player->save(); 
+
+    return redirect()->route('player.edit', $id)->with('success', '選手情報が更新されました。');
+}
 }
